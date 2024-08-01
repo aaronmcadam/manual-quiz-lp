82cf37ccf9b51b2b1dfd52e82d33694640c26edc
Aaron McAdam <aaron@aaronmcadam.com>
2024-08-01 19:19:05 +0100

    add failing e2e test

--- tests/visitor-completes-quiz.spec.ts
+++ tests/visitor-completes-quiz.spec.ts
@@ -3,15 +3,9 @@ import { test, expect } from "@playwright/test";
 test("smoke test", async ({ page }) => {
   await page.goto("/");
 
+  await page.getByRole("button", { name: /take the quiz/i }).click();
+
   await expect(
-    page.getByRole(
-      "button",
-
-      { name: /take the quiz/i },
-    ),
+    page.getByRole("heading", { name: /is manual right for you/i }),
   ).toBeVisible();
-
-  // await expect(
-  //   page.getByRole("heading", { name: /is manual right for you/i }),
-  // ).toBeVisible();
 });
